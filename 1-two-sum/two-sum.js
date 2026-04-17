9718ccabf59a141e27bb53e4779cad153f69b260
priyanka1work <priyanka1workemail@gmail.com>
2026-04-17 23:48:02 +0530

    Time: 2 ms (66.46%) | Memory: 55.3 MB (42.10%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,22 +4,22 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // let map= new Map()
+    let map= new Map()
     
-    // for(i=0;i<nums.length;i++){
-    //     let complement=target-nums[i];
-    //     if(map.has(complement)){
-    //         return [i,map.get(complement)]
-    //     }
-    //     map.set(nums[i],i)
-    // }
-    for(let i=0;i<nums.length-1;i++){
-         for(let j=i+1;j<nums.length;j++){
-             if(nums[i]+nums[j]==target)
-             return [i,j]
+    for(i=0;i<nums.length;i++){
+        let complement=target-nums[i];
+        if(map.has(complement)){
+            return [i,map.get(complement)]
+        }
+        map.set(nums[i],i)
+    }
+    // for(let i=0;i<n-1;i++){
+    //     for(let j=i+1;j<n;j++){
+    //         if(nums[i]+nums[j]==target)
+    //         return [i,j]
 
-       }
-}
+    //     }
+    // }
 
     
 };
